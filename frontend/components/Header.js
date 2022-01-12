314ed9ce6b796b59f0c1e7273ae72494e269d38f
inzimam Ul Haq <inzemamsmile2@gmail.com>
2022-01-13 00:42:15 +0500

    tag and category api

--- frontend/components/Header.js
+++ frontend/components/Header.js
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import { signout, isAuth } from '../actions/auth';
 import Router from 'next/router';
+import NProgress from 'nprogress';
 import Link from 'next/link';
 import { APP_NAME } from '../config';
 import {
@@ -16,6 +17,11 @@ import {
   DropdownMenu,
   DropdownItem
 } from 'reactstrap';
+import '.././node_modules/nprogress/nprogress.css';
+
+Router.onRouteChangeStart = url => NProgress.start();
+Router.onRouteChangeComplete = url => NProgress.done();
+Router.onRouteChangeError = url => NProgress.done();
 
 const Header = () => {
   const [isOpen, setIsOpen] = useState(false);
