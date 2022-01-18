b21f36d8e247ae94b44a092b11d53399c6c5507a
inzimam Ul Haq <inzemamsmile2@gmail.com>
2022-01-18 23:19:28 +0500

    blog pt.2

--- frontend/components/crud/BlogCreate.js
+++ frontend/components/crud/BlogCreate.js
@@ -23,6 +23,12 @@ const CreateBlog = ({ router }) => {
         }
     };
 
+    const [categories, setCategories] = useState([]);
+    const [tags, setTags] = useState([]);
+
+    const [checked, setChecked] = useState([]); // categories
+    const [checkedTag, setCheckedTag] = useState([]); // tags
+
     const [body, setBody] = useState(blogFromLS());
     const [values, setValues] = useState({
         error: '',
@@ -37,11 +43,43 @@ const CreateBlog = ({ router }) => {
 
     useEffect(() => {
         setValues({ ...values, formData: new FormData() });
+        initCategories();
+        initTags();
     }, [router]);
+
+    const initCategories = () => {
+        getCategories().then(data => {
+            if (data.error) {
+                setValues({ ...values, error: data.error });
+            } else {
+                setCategories(data);
+            }
+        });
+    };
+
+    const initTags = () => {
+        getTags().then(data => {
+            if (data.error) {
+                setValues({ ...values, error: data.error });
+            } else {
+                setTags(data);
+            }
+        });
+    };
 
     const publishBlog = e => {
         e.preventDefault();
-        console.log('ready to publishBlog');
+        // console.log('ready to publishBlog');
+        createBlog(formData, token).then(data => {
+            if (data.error) {
+                setValues({ ...values, error: data.error });
+            } else {
+                setValues({ ...values, title: '', error: '', success: `A new blog titled "${data.title}" is created` });
+                setBody('');
+                setCategories([]);
+                setTags([]);
+            }
+        });
     };
 
     const handleChange = name => e => {
@@ -59,6 +97,74 @@ const CreateBlog = ({ router }) => {
             localStorage.setItem('blog', JSON.stringify(e));
         }
     };
+
+    const handleToggle = c => () => {
+        setValues({ ...values, error: '' });
+        // return the first index or -1
+        const clickedCategory = checked.indexOf(c);
+        const all = [...checked];
+
+        if (clickedCategory === -1) {
+            all.push(c);
+        } else {
+            all.splice(clickedCategory, 1);
+        }
+        console.log(all);
+        setChecked(all);
+        formData.set('categories', all);
+    };
+
+    const handleTagsToggle = t => () => {
+        setValues({ ...values, error: '' });
+        // return the first index or -1
+        const clickedTag = checked.indexOf(t);
+        const all = [...checkedTag];
+
+        if (clickedTag === -1) {
+            all.push(t);
+        } else {
+            all.splice(clickedTag, 1);
+        }
+        console.log(all);
+        setCheckedTag(all);
+        formData.set('tags', all);
+    };
+
+    const showCategories = () => {
+        return (
+            categories &&
+            categories.map((c, i) => (
+                <li key={i} className="list-unstyled">
+                    <input onChange={handleToggle(c._id)} type="checkbox" className="mr-2" />
+                    <label className="form-check-label">{c.name}</label>
+                </li>
+            ))
+        );
+    };
+
+    const showTags = () => {
+        return (
+            tags &&
+            tags.map((t, i) => (
+                <li key={i} className="list-unstyled">
+                    <input onChange={handleTagsToggle(t._id)} type="checkbox" className="mr-2" />
+                    <label className="form-check-label">{t.name}</label>
+                </li>
+            ))
+        );
+    };
+
+    const showError = () => (
+        <div className="alert alert-danger" style={{ display: error ? '' : 'none' }}>
+            {error}
+        </div>
+    );
+
+    const showSuccess = () => (
+        <div className="alert alert-success" style={{ display: success ? '' : 'none' }}>
+            {success}
+        </div>
+    );
 
     const createBlogForm = () => {
         return (
@@ -81,13 +187,45 @@ const CreateBlog = ({ router }) => {
         );
     };
 
-    return <div>
-    {createBlogForm()}
-    <hr />
-    {JSON.stringify(title)}
-    <hr />
-    {JSON.stringify(body)}
-        </div>;
+    return (
+        <div className="container-fluid">
+             <div className="row">
+                <div className="col-md-8">
+                    {createBlogForm()}
+                    <div className="pt-3">
+                        {showError()}
+                        {showSuccess()}
+                    </div>
+                </div>
+
+                <div className="col-md-4">
+                    <div>
+                        <div className="form-group pb-2">
+                            <h5>Featured image</h5>
+                            <hr />
+
+                            <small className="text-muted">Max size: 1mb</small>
+                            <label className="btn btn-outline-info">
+                                Upload featured image
+                                <input onChange={handleChange('photo')} type="file" accept="image/*" hidden />
+                            </label>
+                        </div>
+                    </div>
+                    <div>
+                        <h5>Categories</h5>
+                        <hr />
+
+                        <ul style={{ maxHeight: '200px', overflowY: 'scroll' }}>{showCategories()}</ul>
+                    </div>
+                    <div>
+                        <h5>Tags</h5>
+                        <hr />
+                        <ul style={{ maxHeight: '200px', overflowY: 'scroll' }}>{showTags()}</ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
 };
 
 CreateBlog.modules = {
--- frontend/pages/_document.js
+++ frontend/pages/_document.js
@@ -1,5 +1,6 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document';
 
+
 class MyDocument extends Document {
   render() {
     return (
@@ -10,7 +11,7 @@ class MyDocument extends Document {
             rel="stylesheet"
             href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.3.1/css/bootstrap.min.css"
           />
-          <link rel="stylesheet" href="/public/css/styles.css" />
+          <link rel="stylesheet" href='/public/styles.css' />
         </Head>
         <body>
           <Main />
