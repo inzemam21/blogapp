c97c9ce3479d824671c02109cf0262aa50bc0c3b
inzimam Ul Haq <inzemamsmile2@gmail.com>
2022-01-19 23:43:37 +0500

    list all blogs

--- backend/routes/blog.js
+++ backend/routes/blog.js
@@ -5,5 +5,10 @@ const { create } = require('../controllers/blog');
 const { requireSignin, adminMiddleware } = require('../controllers/auth');
 
 router.post('/blog', requireSignin, adminMiddleware, create);
+router.get('/blogs', list);
+router.post('/blogs-categories-tags', listAllBlogsCategoriesTags);
+router.get('/blog/:slug', read);
+router.delete('/blog/:slug', requireSignin, adminMiddleware, remove);
+router.put('/blog/:slug', requireSignin, adminMiddleware, update);
 
 module.exports = router;
